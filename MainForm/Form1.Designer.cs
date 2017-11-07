d7a43922f816272d8802d1ed08da6796a425aa7f
Ilya <aromarich21@yandex.ru>
2017-11-07 16:42:39 +0300

    Add files via upload

--- MainForm/Form1.Designer.cs
+++ MainForm/Form1.Designer.cs
@@ -29,8 +29,25 @@
         private void InitializeComponent()
         {            
             this.lblShowPwd = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.pwdMeter = new CustomControls.PasswordMeter();
             this.pwdTxtBox = new CustomControls.PasswordTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblShowPwd
@@ -41,32 +58,223 @@
             this.lblShowPwd.Size = new System.Drawing.Size(0, 13);
             this.lblShowPwd.TabIndex = 3;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Your password:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Show your pass";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // pwdMeter
             // 
             this.pwdMeter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pwdMeter.Location = new System.Drawing.Point(162, 15);
+            this.pwdMeter.Location = new System.Drawing.Point(38, 331);
             this.pwdMeter.Name = "pwdMeter";
-            this.pwdMeter.Size = new System.Drawing.Size(138, 25);
+            this.pwdMeter.Size = new System.Drawing.Size(210, 20);
             this.pwdMeter.TabIndex = 4;
+            this.pwdMeter.Load += new System.EventHandler(this.pwdMeter_Load);
             // 
             // pwdTxtBox
             // 
-            this.pwdTxtBox.Location = new System.Drawing.Point(12, 18);
+            this.pwdTxtBox.Location = new System.Drawing.Point(38, 65);
             this.pwdTxtBox.Name = "pwdTxtBox";
-            this.pwdTxtBox.Size = new System.Drawing.Size(138, 20);
+            this.pwdTxtBox.Size = new System.Drawing.Size(210, 20);
             this.pwdTxtBox.TabIndex = 0;
             this.pwdTxtBox.TextChanged += new System.EventHandler(this.pwdTxtBox_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Generate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Generation";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(163, 273);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "8",
+            "10",
+            "12"});
+            this.comboBox1.Location = new System.Drawing.Point(163, 164);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(85, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "8";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Length:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Strong",
+            "Very strong"});
+            this.comboBox2.Location = new System.Drawing.Point(163, 191);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(85, 21);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.Text = "Strong";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Difficulty:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(38, 302);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Select password";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(163, 302);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "With symbols:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.comboBox3.Location = new System.Drawing.Point(163, 219);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(85, 21);
+            this.comboBox3.TabIndex = 21;
+            this.comboBox3.Text = "Yes";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Check your pass";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 60);
+            this.ClientSize = new System.Drawing.Size(293, 383);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pwdMeter);
             this.Controls.Add(this.lblShowPwd);
             this.Controls.Add(this.pwdTxtBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheckPass";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +285,23 @@
         private CustomControls.PasswordTextBox pwdTxtBox;
         private System.Windows.Forms.Label lblShowPwd;
         private CustomControls.PasswordMeter pwdMeter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- MainForm/Form1.cs
+++ MainForm/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,27 @@ namespace MainForm
 {
     public partial class Form1 : Form
     {
+        private static string GeneratePassword(int length,bool value)
+        {
+            string pass = "";
+            bool symbolStatus = value;
+            string[] allList = { "0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A","B", "C", "D","E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b", "c", "d", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "U", "Y", "a", "e", "i", "o", "u", "y" , "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", ";", ":", ",", ".", "/", "?","|", "`", "~", "[", "]", "{", "}"};
+            string[] charNumList = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b", "c", "d", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "U", "Y", "a", "e", "i", "o", "u", "y" };
+            Random symbol = new Random();
+            if (symbolStatus)
+            {
+                for (int count = 0; count < length; count++)
+                {
+                    pass = pass + allList[symbol.Next(allList.Length)];
+                }
+            }
+            else
+                for (int count = 0; count < length; count++)
+                {
+                    pass = pass + charNumList[symbol.Next(charNumList.Length)];
+                }
+            return pass;
+        }
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +42,94 @@ namespace MainForm
         private void pwdTxtBox_TextChanged(object sender, EventArgs e)
         {
             pwdMeter.SetPassword(pwdTxtBox.Password);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = pwdTxtBox.Password;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pwdMeter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pwdMeter.SetPassword("");
+            //pwdTxtBox.Password.Remove(Convert.ToInt32(comboBox1.Text));
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            bool statusSymbol;
+            int length = Convert.ToInt32(comboBox1.Text);
+            
+            if (comboBox3.Text == "Yes")
+                statusSymbol = true;
+            else
+                statusSymbol = false;
+            string pass = GeneratePassword(length,statusSymbol);
+            int score = PasswordUtils.ComputePasswordScore(pass);
+            if (comboBox2.Text == "Strong")
+                while (score < 59 || score > 79)
+                {
+                    pass = GeneratePassword(length, statusSymbol);
+                    score = PasswordUtils.ComputePasswordScore(pass);
+                }
+            else
+                while (score < 80)
+                {
+                    pass = GeneratePassword(length, statusSymbol);
+                    score = PasswordUtils.ComputePasswordScore(pass);
+                }        
+            pwdMeter.SetPassword(pass);
+            textBox2.Text = pass;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pwdMeter.SetPassword("");
+            textBox2.Clear();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.SelectionStart = 0;
+            textBox2.SelectionLength = textBox2.Text.Length;
+            textBox2.Focus();
+            
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
